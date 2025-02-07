f209b972d516741edbd98e18c5945f297946dfe8
surajitmandal639 <surajitmandal639@gmail.com>
2025-02-07 13:32:04 +0530

    tst

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::get('/dashboard', [DashboardController::class, 'dashboard'])->middleware(
 
 // Memberships
 Route::middleware(['auth', 'role:user'])->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
